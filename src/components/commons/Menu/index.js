a43f1e8717d481517c9989f8d6cd0da22b6c8581
Claudio <claudiomeirabs@gmail.comm>
2021-02-11 17:13:35 -0300

    Final da aula 05

--- src/components/commons/Menu/index.js
+++ src/components/commons/Menu/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Logo } from "../../../theme/Logo"
+import Text from "../../foundation/Text"
 import { Button } from "../Button"
 import { MenuWrapper } from "./styles/MenuWrapper"
 
@@ -27,9 +28,9 @@ export default function Menu() {
         {links.map((link) => {
           return (
             <li key={link.url}>
-              <a href={link.url}>
+              <Text variant="smallestException" tag="a" href={link.url}>
                 {link.texto}
-              </a>
+              </Text>
             </li>
           )
         } )}
---
+++ src/components/foundation/Text/index.js
@@ -0,0 +1,47 @@
+import React from "react"
+import PropTypes from "prop-types"
+import styled, { css } from "styled-components"
+
+export const TextStyleVariantsMap = {
+paragraph1: css`
+font-size: ${({ theme }) => theme.typographyVariants.paragraph1.fontSize};
+font-weight: ${({ theme }) => theme.typographyVariants.paragraph1.fontWeight};
+line-height: ${({ theme }) => theme.typographyVariants.paragraph1.lineHeight};
+`,
+smallestException: css`
+font-size: ${({ theme }) => theme.typographyVariants.smallestException.fontSize};
+font-weight: ${({ theme }) => theme.typographyVariants.smallestException.fontWeight};
+line-height: ${({ theme }) => theme.typographyVariants.smallestException.lineHeight};
+`,
+}
+
+
+
+
+const TextBase = styled.span`
+${(props) => { return TextStyleVariantsMap[props.variant]}}
+`
+
+export default function Text ({tag, variant, children}) {
+  return (
+
+    <TextBase
+      as={tag}
+      variant={variant}
+    >
+      {children}
+    </TextBase>
+
+  )
+}
+
+Text.propTypes = {
+  tag: PropTypes.string.isRequired,
+  variant: PropTypes.string,
+  children: PropTypes.node.isRequired,
+}
+
+Text.defaultsProps = {
+  tag: "span",
+  variant: "paragraph1",
+}
